08ae59d99b104d324f7e41c8db363e3f7fddb964
henriqlimac <h.cardosinho.lima@gmail.com>
2026-03-11 00:12:00 -0300

    feat: created 'Seller' and 'Department' DAOs

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller payload);
+    void update(Seller payload);
+    void deleteById(Integer id);
+
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
